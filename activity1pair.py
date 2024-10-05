0ff87106d9d97f81546360b6761fb083ff6c7683
mayjel <maijalesalva@gmail.com>
2024-10-05 10:38:44 +0800

    patotoya

--- activity1pair.py
+++ activity1pair.py
@@ -3,4 +3,14 @@ number = []
 
 for x in range(number):
     ask = input("numbers?:")
-    number
+    number .append(ask)
+
+    print(numbers)
+
+    for number in numbers:
+        if number > "0" :
+            print(number + "positive")
+        elif number < "0":
+            print(number + "negative")
+        else:
+            print("ERRRNGK")
